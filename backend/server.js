e6acd016391592e296bedab8c9c3a8df166a2e2a
Marco Manunta <mar.manunta@gmail.com>
2024-03-16 16:02:17 +0100

    basic express server setup

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,11 @@
-console.log('Server...')
+const express = require('express')
+require('dotenv').config()
+const PORT = process.env.PORT || 8000
+
+const app = express()
+
+app.get('/users', (req, res) => {
+  res.status(200).json({ message: 'Welcome to Support Desk API' })
+})
+
+app.listen(PORT, console.log(`Server started on port ${PORT}`))
