2b054c418733d0f7bc2247a6cc52949d8e98096b
Marco Manunta <mar.manunta@gmail.com>
2024-03-17 21:51:57 +0100

    protect routes & authentication

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -48,7 +48,7 @@ const registerUser = asyncHandler(async (req, res) => {
   }
 })
 
-// @desc     Login a new
+// @desc     Login a user
 // @route    /api/users/login
 // @access   Public
 const loginUser = asyncHandler(async (req, res) => {
@@ -70,6 +70,18 @@ const loginUser = asyncHandler(async (req, res) => {
   }
 })
 
+// @desc     Get current user
+// @route    /api/users/me
+// @access   Private
+const getMe = asyncHandler(async (req, res) => {
+  const user = {
+    id: req.user._id,
+    email: req.user.email,
+    name: req.user.name,
+  }
+  res.status(200).json(user)
+})
+
 // Generate token
 const generateToken = id => {
   return jwt.sign({ id }, process.env.JWT_SECRET, {
@@ -80,4 +92,5 @@ const generateToken = id => {
 module.exports = {
   registerUser,
   loginUser,
+  getMe,
 }
